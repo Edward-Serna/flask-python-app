8a663f325d2e1d8663b163e6c7a3b0232c82d308
Edward Serna <edward.serna@my.utsa.edu>
2025-01-13 13:35:42 -0600

    added user and Machine Name

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,22 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)  
 
-# import datetime
-# import time 
-# x = datetime.datetime.now()
+def load_users():
+    try:
+        with open('users.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
-@app.route('/username/<x>')
-def setUsername(x):
-    return {
-        'username': x
-    }
+def save_users(users):
+    with open('users.json', 'w') as file:
+        json.dump(users, file)
 
-# @app.route('/MachineName')
-# def get_time():
-#     return {
-#         'time': time.time()
-#     }
+@app.route("/addUser", methods=["POST"])
+
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    machineName = data.get('machineName')
+    users = load_users()
+    users.append({"username": username, "MachineName": machineName})
+    save_users(users)
+
+    return jsonify({"message": "User added successfully!", "username": username, "MachineName": machineName}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,71 +1,63 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [username, setUsername] = useState('');
-  // const [machineName, setMachineName] = useState("");
-
-  // useEffect(() => {
-  // 	fetch('/date').then(res => res.json()).then(data => {
-  // 		setCurrentDate(data.date);
-  // 	});
-  // }, []);
-
-  useEffect(() => {
-
-  }, []);
-
-  // useEffect(() => {
-  // 	fetch('/time').then(res => res.json()).then(data => {
-  // 		setMachineName(data.time);
-  // 	});
-  // }, []);
-
-  // useEffect(() => {
-  // 	fetch('/time').then(res => res.json()).then(data => {
-  // 		setMachineName(data.time);
-  // 	});
-  // }, []);
+  const [username, setUsername] = useState("");
+  const [machineName, setMachineName] = useState("");
 
   return (
     <div className="Page">
       <img className="img" alt="logo" src="MF_Logo.svg" />
       <div className="Card">
         <h2>Welcome</h2>
-        <p>Please complete these steps blah blah blah explanation about this current set of  fields that they need to fill out blah blah blah</p>
+        <p>Please complete these steps blah blah blah explanation about this current set of fields that they need to fill out blah blah blah</p>
       </div>
       <div className="Card">
         <h2>First Time Setup</h2>
         <div className="CardItem">
           <p>Username: </p>
-          <input type='text' />
+          <input
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            type="text"
+          />
         </div>
         <div className="CardItem">
           <p>Machine Name: </p>
-          <input type='text' />
-        </div >
+          <input
+           value={machineName}
+           onChange={(event) => setMachineName(event.target.value)}
+           type="text" 
+           />
+        </div>
         <div className="LargeInput">
           <p>SSH Key: </p>
-          <textarea type='text' placeholder="Begins with ‘ssh-rsa’, ‘ecdsa-sha2-nistp256’, ‘ecdsa-sha2-nistp384’, ‘ecdsa-sha2-nistp521’, ‘ssh-ed25519’, ‘sk-ecdsa-sha2-nistp256@openssh.com’, or ‘sk-ssh-ed25519@openssh.com’" />
+          <textarea
+            type="text"
+            placeholder="Begins with ‘ssh-rsa’, ‘ecdsa-sha2-nistp256’, etc."
+          />
         </div>
         <div className="Button">
-          <form>
-            <button
-              type="submit"
-              value="Add Todo"
-              onClick={async () => {
-                const todo = { username };
-                const response = await fetch("/add_todo", {
-                  method: "POST",
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify(todo)
-                })
-              }}>
-            </button>
-          </form>
+          <button
+            onClick={async () => {
+              const response = await fetch('http://localhost:5000/addUser', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, machineName }),
+              });
+
+              if (response.ok) {
+                const result = await response.json();
+                console.log(result.message);
+              } else {
+                console.error("Failed to add user");
+              }
+            }}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
