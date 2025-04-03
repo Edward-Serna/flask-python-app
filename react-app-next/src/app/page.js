7f7a23796440a5a98a3bce7a0690eebb7209118f
Neel Mittal <neel.mittal101@gmail.com>
2025-04-03 12:04:29 -0500

    LOCALHOST

--- react-app-next/src/app/page.js
+++ react-app-next/src/app/page.js
@@ -14,7 +14,7 @@ const Home = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://192.168.1.107:5000/addUser", {
+      const response = await fetch("http://localhost:5000/addUser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
