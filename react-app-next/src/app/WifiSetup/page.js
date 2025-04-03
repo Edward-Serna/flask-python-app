5d9e0d38c60b841be7e8e309c2904c356fb38606
Neel Mittal <neel.mittal101@gmail.com>
2025-04-03 12:21:56 -0500

    Update page.js

--- react-app-next/src/app/WifiSetup/page.js
+++ react-app-next/src/app/WifiSetup/page.js
@@ -24,7 +24,7 @@ const WifiSetup = () => {
   }, [selectedNetwork]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/networks')
+    fetch('http://10.42.0.1:5000/networks')
       .then(response => response.json())
       .then(data => {
         setWifi(data.networks || []);
